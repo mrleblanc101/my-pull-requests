3248845bf37594073a46cfd9ec4e82fc3678140f
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2024-08-23 10:47:51 -0400

    Deploy to github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,16 +7,16 @@ export default defineNuxtConfig({
 
   // https://nuxt.com/modules
   modules: [
-    '@nuxthub/core',
+    // '@nuxthub/core',
     '@nuxt/eslint',
     '@nuxt/ui',
     '@vueuse/nuxt',
   ],
 
-  // https://hub.nuxt.com/docs/getting-started/installation#options
-  hub: {
-    cache: true,
-  },
+//   // https://hub.nuxt.com/docs/getting-started/installation#options
+//   hub: {
+//     cache: true,
+//   },
 
   // https://eslint.nuxt.com
   eslint: {
